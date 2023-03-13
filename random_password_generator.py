e8345e0395177f0c0bb396b8a2f34d3fbb101e1e
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-03-13 17:23:12 +0545

    Add files via upload
    A simple python code to generate strong password. This code takes total number of pw as input from the user, and gives out pw in the form of output.

---
+++ random_password_generator.py
@@ -0,0 +1,21 @@
+import string
+import random
+
+
+def password():
+    a = string.ascii_uppercase
+    b = string.ascii_lowercase
+    c = string.digits
+    d = string.punctuation
+    passlength = int(input("Enter the password length : \n"))
+    e = []
+    e.extend(list(a))
+    e.extend(list(b))
+    e.extend(list(c))
+    e.extend(list(d))
+    random.shuffle(e)
+    passs = (''.join(e[0:passlength]))
+    print(passs)
+
+
+password()
