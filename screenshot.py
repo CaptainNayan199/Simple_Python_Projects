1102f33703b769355669007e67e45a78a20ae65e
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-03-13 17:28:38 +0545

    Add files via upload
    This is a bit complex python code that generates a simple window from where user can either take screenshot of their pc or exit the application. I have used some popular python packages like pyautogui, and obviously tkinter. Any changes if you think can be made onto this project, please feel free to submit a pull request. Will appreciate it.

---
+++ screenshot.py
@@ -0,0 +1,40 @@
+import pyautogui
+import time
+from random import randint
+import tkinter as neha
+
+
+def screenshot():
+    name = (randint(10, 900) * 1000)
+    name = 'E:/PythonProjects/screenshot image/{}.png'.format(name)
+    time.sleep(0)
+    img = pyautogui.screenshot(name)
+    img.show()
+
+
+# screenshot()
+
+root = neha.Tk()
+root.title("Hello Beautiful People! Please Take Screenshot")
+root.geometry("600x400")
+frame = neha.Frame(root)
+frame.pack()
+button = neha.Button(
+    frame,
+    text="Take SS",
+    command=screenshot
+)
+
+button.pack(side=neha.LEFT)
+
+close = neha.Button(
+    frame,
+    text='Quit',
+    command=quit
+)
+close.pack(side=neha.RIGHT)
+
+root.mainloop()
+
+
+# screenshot taking applicatio using in python
