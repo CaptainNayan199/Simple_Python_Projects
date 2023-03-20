8e3eaf52141159665af49ac1940eed6b0e2d445c
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-03-20 20:14:55 +0545

    Create random_num.py
    This is simple random number generator python code. It can be beneficial in many ways like generating a random OTP numbers, unique id and registration number, sending numerals code in email verification, and many more.

---
+++ random_num.py
@@ -0,0 +1,10 @@
+from random inport randint
+
+a = randint(10, 99)*9275
+print(a)
+
+
+# or you can do this way too
+
+print(randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9), sep="") #this will automatically generate the new 
+# 6 digit random number, using nothing as a seperator(tab, comma, etc can be used as well). This random number can be used in many ways.
