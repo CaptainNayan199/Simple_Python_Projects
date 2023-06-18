7f1a427b71504045a3515b37d1f5c46b7a43ba95
Captain Nayan <105142730+CaptainNayan199@users.noreply.github.com>
2023-06-18 18:27:08 +0545

    files updated

--- analog_clock.py
+++ analog_clock.py
@@ -22,3 +22,25 @@ def clock_logic():
     # Hours numbers
     for i in range(12):
         angle = i * math.pi / 6 - math.pi / 2
+        x = 400 / 2 + 0.7 * 400 / 2 * math.cos(angle)
+        y = 400 / 2 + 0.7 * 400 / 2 * math.sin(angle)
+        if i == 0:
+            canvas.create_text(x, y - 10, text=str(i + 12), font=("Helvetica", 12))
+        else:
+            canvas.create_text(x, y, text=str(i), font=("Helvetica", 12))
+
+    # Minute lines
+
+    for i in range(60):
+        angle = i * math.pi / 30 - math.pi / 2
+        x1 = 400 / 2 + 0.8 * 400 / 2 * math.cos(angle)
+        y1 = 400 / 2 + 0.8 * 400 / 2 * math.sin(angle)
+        x2 = 400 / 2 + 0.9 * 400 / 2 * math.cos(angle)
+        y2 = 400 / 2 + 0.9 * 400 / 2 * math.sin(angle)
+        if i % 5 == 0:
+            canvas.create_line(x1, y1, x2, y2, fill="black", width=2)
+        else:
+            canvas.create_line(x1, y1, x2, y2, fill="black", width=1)
+
+
+
